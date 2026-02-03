93929d1c35843ffb2ff7d0125c8f855ef8e58ab9
Trần Ngọc Thanh Tùng <tungtnt.23ite@vku.udn.vn>
2026-02-03 19:54:01 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -63,7 +63,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://manage-recruitment-api.vercel.app",
     },
     (accessToken, refreshToken, profile, done) => {
       const googleId = profile.id;
